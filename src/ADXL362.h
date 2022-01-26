61cf0253d7f0371b2cd5ec0cff9cd03825f1c5a2
winkelict <98483683+winkelict@users.noreply.github.com>
2022-01-26 20:06:51 +0100

    added licensing to source files

--- src/ADXL362.h
+++ src/ADXL362.h
@@ -1,3 +1,29 @@
+/*
+
+Winkel ICT ADXL362
+
+Library for ADXL362 accelerometer: ultralow power (0,270ua) with autonomous motion switch functionality.
+
+Copyright (C) 2022 Klaas-Jan Winkel / Winkel ICT
+
+All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
 #ifndef __ADXL362_H__
 #define __ADXL362_H__
 
